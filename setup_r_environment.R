0c6462e301696a707571d28a6497e89a44406e45
Zahra Qureshi <21babba156@sls.pdpu.ac.in>
2026-05-27 17:13:55 +0100

    Align config and folder structure with thesis research design

--- setup_r_environment.R
+++ setup_r_environment.R
@@ -1,3 +1,5 @@
+options(repos = c(CRAN = "https://cloud.r-project.org"))
+
 if (!requireNamespace("renv", quietly = TRUE)) install.packages("renv")
 renv::init()
 packages <- c("tidyverse","lubridate","tseries","forecast","xts","zoo","httr2","jsonlite","readxl","here","glue","tictoc")
